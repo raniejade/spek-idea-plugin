f58bda09b8d8fa28ac014788930044368b3d2bff
Ranie Jade Ramiso <raniejaderamiso@gmail.com>
2016-07-18 11:04:39 +0800

    Setup UI

--- src/main/kotlin/org/jetbrains/spek/idea/SpekRunConfiguration.kt
+++ src/main/kotlin/org/jetbrains/spek/idea/SpekRunConfiguration.kt
@@ -1,6 +1,9 @@
 package org.jetbrains.spek.idea
 
+import com.intellij.diagnostic.logging.LogConfigurationPanel
+import com.intellij.execution.CommonJavaRunConfigurationParameters
 import com.intellij.execution.Executor
+import com.intellij.execution.JavaRunConfigurationExtensionManager
 import com.intellij.execution.configurations.ConfigurationFactory
 import com.intellij.execution.configurations.JavaRunConfigurationModule
 import com.intellij.execution.configurations.ModuleBasedConfiguration
@@ -8,6 +11,8 @@ import com.intellij.execution.configurations.RunProfileState
 import com.intellij.execution.runners.ExecutionEnvironment
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.module.ModuleManager
+import com.intellij.openapi.options.SettingsEditor
+import com.intellij.openapi.options.SettingsEditorGroup
 import com.intellij.openapi.util.JDOMExternalizerUtil
 import org.jdom.Element
 import java.util.*
@@ -16,16 +21,61 @@ import java.util.*
  * @author Ranie Jade Ramiso
  */
 class SpekRunConfiguration(javaRunConfigurationModule: JavaRunConfigurationModule, factory: ConfigurationFactory, name: String)
-: ModuleBasedConfiguration<JavaRunConfigurationModule>(name, javaRunConfigurationModule, factory) {
+: ModuleBasedConfiguration<JavaRunConfigurationModule>(name, javaRunConfigurationModule, factory), CommonJavaRunConfigurationParameters {
+
+    data class Data(
+        var spec: String,
+        var scope: String,
+        var alternativeJrePath: String,
+        var envs: MutableMap<String, String>,
+        var isPassParentEnvs: Boolean,
+        var runClass: String,
+        var workingDirectory: String?,
+        var programParameters: String?,
+        var vmParameters: String,
+        var isAlternativeJrePathEnabled: Boolean
+    )
 
     override fun getValidModules(): MutableCollection<Module> {
         return Arrays.asList(*ModuleManager.getInstance(project).modules)
     }
 
-    var filter = ""
-    var query = ""
+    private val data = Data(
+        "",
+        "",
+        "",
+        mutableMapOf(),
+        false,
+        "",
+        "",
+        "",
+        "",
+        false
+    )
 
-    override fun getConfigurationEditor() = SpekSettingsEditor(project)
+    var spec: String
+        get() {
+            return data.spec
+        }
+        set(value) {
+            data.spec = value
+        }
+
+    var scope: String
+        get() {
+            return data.scope
+        }
+        set(value) {
+            data.scope = value
+        }
+
+    override fun getConfigurationEditor(): SettingsEditor<SpekRunConfiguration> {
+        return SettingsEditorGroup<SpekRunConfiguration>().apply {
+            addEditor("Configuration", SpekSettingsEditor(project))
+            JavaRunConfigurationExtensionManager.getInstance().appendEditors(this@SpekRunConfiguration, this)
+            addEditor("Logs", LogConfigurationPanel())
+        }
+    }
 
     override fun getState(executor: Executor, environment: ExecutionEnvironment): RunProfileState? {
         throw UnsupportedOperationException()
@@ -34,14 +84,62 @@ class SpekRunConfiguration(javaRunConfigurationModule: JavaRunConfigurationModul
     override fun writeExternal(element: Element) {
         super.writeExternal(element)
         writeModule(element)
-        JDOMExternalizerUtil.writeField(element, "filter", filter)
-        JDOMExternalizerUtil.writeField(element, "query", query)
+        JDOMExternalizerUtil.writeField(element, "spec", spec)
+        JDOMExternalizerUtil.writeField(element, "scope", scope)
     }
 
     override fun readExternal(element: Element) {
         super.readExternal(element)
         readModule(element)
-        filter = JDOMExternalizerUtil.readField(element, "filter", "")
-        query = JDOMExternalizerUtil.readField(element, "query", "")
+        spec = JDOMExternalizerUtil.readField(element, "spec", "")
+        scope = JDOMExternalizerUtil.readField(element, "scope", "")
+    }
+
+    override fun setAlternativeJrePath(path: String) {
+        data.alternativeJrePath = path
+    }
+
+    override fun getEnvs(): MutableMap<String, String> = data.envs
+
+    override fun isPassParentEnvs() = data.isPassParentEnvs
+
+    override fun getRunClass() = data.runClass
+
+    override fun getWorkingDirectory() = data.workingDirectory
+
+    override fun setPassParentEnvs(passParentEnvs: Boolean) {
+        data.isPassParentEnvs = passParentEnvs
+    }
+
+    override fun getProgramParameters() = data.programParameters
+
+    override fun getAlternativeJrePath() = data.alternativeJrePath
+
+    override fun setVMParameters(value: String) {
+        data.vmParameters = value
+    }
+
+    override fun setProgramParameters(value: String?) {
+        data.programParameters = value
+    }
+
+    override fun isAlternativeJrePathEnabled() = data.isAlternativeJrePathEnabled
+
+    override fun getPackage(): String {
+        TODO()
+    }
+
+    override fun setAlternativeJrePathEnabled(enabled: Boolean) {
+        data.isAlternativeJrePathEnabled = enabled
+    }
+
+    override fun getVMParameters() = data.vmParameters
+
+    override fun setWorkingDirectory(value: String?) {
+        data.workingDirectory = value
+    }
+
+    override fun setEnvs(envs: MutableMap<String, String>) {
+        data.envs = envs
     }
 }
--- src/main/kotlin/org/jetbrains/spek/idea/SpekSettingsEditor.kt
+++ src/main/kotlin/org/jetbrains/spek/idea/SpekSettingsEditor.kt
@@ -1,11 +1,13 @@
 package org.jetbrains.spek.idea
 
 import com.intellij.application.options.ModulesComboBox
-import com.intellij.execution.ui.ClassBrowser
+import com.intellij.execution.ui.*
 import com.intellij.ide.util.ClassFilter
 import com.intellij.openapi.fileTypes.PlainTextLanguage
+import com.intellij.openapi.module.Module
 import com.intellij.openapi.options.SettingsEditor
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.LabeledComponent
 import com.intellij.psi.JavaCodeFragment
 import com.intellij.psi.JavaPsiFacade
 import com.intellij.psi.PsiClass
@@ -20,43 +22,59 @@ import javax.swing.JPanel
  */
 class SpekSettingsEditor(val project: Project): SettingsEditor<SpekRunConfiguration>() {
     lateinit var panel: JPanel
-    lateinit var module: ModulesComboBox
-    lateinit var filter: EditorTextFieldWithBrowseButton
-    lateinit var query: TextFieldWithHistory
+    lateinit var commonJavaParameters: CommonJavaParametersPanel
+    lateinit var module: LabeledComponent<ModulesComboBox>
+    lateinit var jrePathEditor: JrePathEditor
+    lateinit var spec: LabeledComponent<EditorTextFieldWithBrowseButton>
+    lateinit var scope: LabeledComponent<TextFieldWithHistory>
 
+    lateinit var moduleSelector: ConfigurationModuleSelector
 
-    override fun resetEditorFrom(configuration: SpekRunConfiguration) {
-        module.selectedModule = configuration.configurationModule.module
-        filter.text = configuration.filter
-        query.text = configuration.query
-    }
-
-    override fun applyEditorTo(configuration: SpekRunConfiguration) {
-        configuration.setModule(module.selectedModule)
-        configuration.filter = filter.text
-        configuration.query = query.text
-    }
-
-    override fun createEditor() = panel
-
-    private fun createUIComponents() {
-        module = ModulesComboBox()
-        module.fillModules(project)
-
-        query = TextFieldWithHistory()
-        filter = EditorTextFieldWithBrowseButton(
-            project,
-            true,
-            JavaCodeFragment.VisibilityChecker.EVERYTHING_VISIBLE,
-            PlainTextLanguage.INSTANCE.associatedFileType
-        )
-
-        val scope: GlobalSearchScope
-        if (module.selectedModule == null) {
-            scope = GlobalSearchScope.allScope(project)
-        } else {
-            scope = GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(module.selectedModule!!)
+    private val searchScope: GlobalSearchScope
+        get() {
+            return if (selectedModule == null) {
+                GlobalSearchScope.allScope(project)
+            } else {
+                GlobalSearchScope.moduleWithDependenciesAndLibrariesScope(selectedModule!!)
+            }
         }
+
+    private var selectedModule: Module?
+        get() {
+            return module.component.selectedModule
+        }
+        set(value) {
+            module.component.selectedModule = value
+        }
+
+    private var selectedSpec: String
+        get() {
+            return spec.component.text
+        }
+        set(value) {
+            spec.component.text = value
+        }
+
+    private var selectedScope: String
+        get() {
+            return scope.component.text
+        }
+        set(value) {
+            scope.component.text = value
+        }
+
+    init {
+        module.component.fillModules(project)
+        moduleSelector = ConfigurationModuleSelector(project, module.component)
+        jrePathEditor.setDefaultJreSelector(DefaultJreSelector.fromModuleDependencies(module.component, false))
+        commonJavaParameters.setModuleContext(selectedModule)
+        commonJavaParameters.setHasModuleMacro()
+        module.component.addActionListener {
+            commonJavaParameters.setModuleContext(selectedModule)
+        }
+
+        val spekCls = PsiClassType.getTypeByName("org.jetbrains.spek.api.Spek", project, searchScope)
+        val subjectSpekCls = PsiClassType.getTypeByName("org.jetbrains.spek.api.SubjectSpek", project, searchScope)
 
 
         val browser = object: ClassBrowser(project, "") {
@@ -64,12 +82,12 @@ class SpekSettingsEditor(val project: Project): SettingsEditor<SpekRunConfigurat
 
 
                 return object: ClassFilter.ClassFilterWithScope {
-                    override fun getScope() = scope
+                    override fun getScope() = searchScope
 
                     override fun isAccepted(aClass: PsiClass): Boolean {
-                        return aClass.superTypes.contains(
-                            PsiClassType.getTypeByName("io.polymorphicpanda.kspec.KSpec", project, scope)
-                        )
+                        return aClass.superTypes.filter {
+                            it == spekCls || it.rawType() == subjectSpekCls
+                        }.any()
                     }
                 }
             }
@@ -77,12 +95,43 @@ class SpekSettingsEditor(val project: Project): SettingsEditor<SpekRunConfigurat
 
             override fun findClass(className: String): PsiClass? {
                 return JavaPsiFacade.getInstance(project)
-                    .findClass(className, scope)
+                    .findClass(className, searchScope)
             }
-
         }
 
-        browser.setField(filter)
+        browser.setField(spec.component)
+    }
+
+
+    override fun resetEditorFrom(configuration: SpekRunConfiguration) {
+        selectedModule = configuration.configurationModule.module
+        selectedSpec = configuration.spec
+        selectedScope = configuration.scope
+        moduleSelector.reset(configuration)
+    }
+
+    override fun applyEditorTo(configuration: SpekRunConfiguration) {
+        configuration.setModule(selectedModule)
+        configuration.spec = selectedSpec
+        configuration.scope = selectedScope
+        moduleSelector.applyTo(configuration)
+        commonJavaParameters.applyTo(configuration)
+    }
+
+    override fun createEditor() = panel
+
+    private fun createUIComponents() {
+        scope = LabeledComponent.create(
+            TextFieldWithHistory(), "Scope", "West"
+        )
+        scope.isEnabled = false
+
+        spec = LabeledComponent.create(EditorTextFieldWithBrowseButton(
+            project,
+            true,
+            JavaCodeFragment.VisibilityChecker.EVERYTHING_VISIBLE,
+            PlainTextLanguage.INSTANCE.associatedFileType
+        ), "Spec", "West")
 
     }
 }
